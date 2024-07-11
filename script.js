40319233a24a36aecf6f116a76a6dc80ccf86e32
nehat06 <nehatarale2001@gmail.com>
2024-07-11 19:34:39 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+//objects 
+//       
+
+let info3 = {
+    // prop:value
+    firstName:"neha",
+    lastName:"ruchi",
+    age:45,
+    rollNo:23
+}
+for(let prop in info3){
+    console.log(prop,info3[prop])
+}
